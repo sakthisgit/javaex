5a918072ac5204ef809864648fa6b16e1d105b9e
DhanalakshmiSiva <63140290+DhanalakshmiSiva@users.noreply.github.com>
2020-06-22 08:41:22 +0530

    Thread

---
+++ Dhanalakshmi_DBDA/WithInterface_ThreadClass.java
@@ -0,0 +1,27 @@
+class Runner implements Runnable{
+    @Override
+    public void run()
+    {
+    for(int i=0;i<10;i++)
+        System.out.println("Runner thread: "+toString()+"\t"+i);
+    try
+    {
+        Thread.sleep(100);
+    }
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+    }
+}
+
+public class WithInterface_ThreadClass {
+    public static void main(String[] args)
+    {
+        Thread t1 = new Thread(new Runner());
+        Thread t2 = new Thread(new Runner());
+        t1.start();
+        t2.start();
+    }
+    
+}
