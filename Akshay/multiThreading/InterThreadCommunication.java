51904e3ca512678f72c29848acbcee9eeac6a740
ak.arane1997 <ak.arane1997@gmail.com>
2020-06-24 11:32:07 +0530

    multiThreading

---
+++ Akshay/multiThreading/InterThreadCommunication.java
@@ -0,0 +1,103 @@
+package multiThreading;
+class Resource{       
+
+    String resourceName;
+    boolean lock = false;  
+
+
+
+    public synchronized String get()    
+    {
+          while (lock == false){     
+                try{
+                      wait();
+                }
+                catch (InterruptedException ie){}                 
+          }
+
+
+           try{
+               Thread.sleep(1000);
+
+           }catch(Exception e){}
+          
+       
+           lock = false;             
+          notifyAll();           
+          return resourceName;
+    }
+
+    public synchronized void put(String name)   
+    {
+          while (lock == true)            
+          {
+                try
+                {
+                      wait();
+                     
+                }
+                catch (InterruptedException ie){}
+
+                try{
+                    Thread.sleep(1000);
+
+                }catch(Exception e){}
+               
+          }
+          System.out.println("Producer Thread" + name);
+          resourceName = name;
+          lock = true;
+          notifyAll();
+    }
+
+
+}
+
+
+class Producer extends Thread{              
+
+   Resource r;
+
+   public Producer(Resource r){
+           this.r = r;
+   }
+
+   public  void run(){
+
+       
+       for(int i=0;i<10;i++){            
+           r.put("Value = "+i);
+       }
+
+
+   }
+
+}
+
+
+class Consumer1 extends Thread{          
+
+   Resource r;
+
+   public Consumer1(Resource r){
+           this.r = r;
+   }
+
+   public void run(){
+
+       for(int i=0;i<10;i++){
+           System.out.println("Consumer Thread"+r.get());  
+       }
+   }
+
+}
+
+public class InterThreadCommunication {
+	 public static void main(String[] args){
+	       Resource r = new Resource();  
+	       Producer p = new Producer(r);   
+	       Consumer1 c = new Consumer1(r);   
+	       p.start();
+	       c.start();
+	   }
+}
