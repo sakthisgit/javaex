6db02aa887081ea2b93d4f0b613a722aac9a396b
rupin123 <rupindas1996@gmail.com>
2020-06-27 15:43:56 +0530

    collection

---
+++ Rupin/collection/MyClass.java
@@ -0,0 +1,17 @@
+// Import the HashMap class
+import java.util.HashMap;
+
+public class MyClass {
+  public static void main(String[] args) {
+    // Create a HashMap object called capitalCities
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+    // Add keys and values (Country, City)
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities);
+  }
+}
+
